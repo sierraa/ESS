80e53b55be3d9ab2c2b74c584d66543dba74d441
augustjanse <augustj@kth.se>
2016-05-08 12:05:30 +0200

    Make getEmail return email and optimize imports

--- app/src/main/java/com/example/sierra/evensimplersyndication/LoginActivity.java
+++ app/src/main/java/com/example/sierra/evensimplersyndication/LoginActivity.java
@@ -4,24 +4,22 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.app.AlertDialog;
+import android.app.LoaderManager.LoaderCallbacks;
 import android.content.Context;
+import android.content.CursorLoader;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
 import android.content.Loader;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
@@ -52,16 +50,10 @@ import static android.Manifest.permission.READ_CONTACTS;
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
-    String BASE_URL ="http://ec2-54-173-215-12.compute-1.amazonaws.com";
-    int USER_ID = -1;
-    String MY_DISPLAY_NAME = null;
-    // Instantiate the RequestQueue.
-    RequestQueue queue = null;
     /**
      * Id to identity READ_CONTACTS permission request.
      */
     private static final int REQUEST_READ_CONTACTS = 0;
-
     /**
      * A dummy authentication store containing known user names and passwords.
      * TODO: remove after connecting to a real authentication system.
@@ -69,6 +61,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
     };
+    String BASE_URL ="http://ec2-54-173-215-12.compute-1.amazonaws.com";
+    int USER_ID = -1;
+    String MY_DISPLAY_NAME = null;
+    // Instantiate the RequestQueue.
+    RequestQueue queue = null;
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
@@ -213,6 +210,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             Intent intent = new Intent(this, MainActivity.class);
             intent.putExtra("USER_ID", USER_ID);
             intent.putExtra("MY_DISPLAY_NAME", MY_DISPLAY_NAME);
+            intent.putExtra("EMAIL", mEmailView.getText());
             startActivity(intent);
         }
     }
@@ -393,6 +391,16 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     }
 
+    private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
+        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<>(LoginActivity.this,
+                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
+
+        mEmailView.setAdapter(adapter);
+    }
+
+
     private interface ProfileQuery {
         String[] PROJECTION = {
                 ContactsContract.CommonDataKinds.Email.ADDRESS,
@@ -401,16 +409,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         int ADDRESS = 0;
         int IS_PRIMARY = 1;
-    }
-
-
-    private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
-        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(LoginActivity.this,
-                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
-
-        mEmailView.setAdapter(adapter);
     }
 
     /**
--- app/src/main/java/com/example/sierra/evensimplersyndication/MainActivity.java
+++ app/src/main/java/com/example/sierra/evensimplersyndication/MainActivity.java
@@ -1,39 +1,31 @@
 package com.example.sierra.evensimplersyndication;
 
 import android.content.Intent;
-import android.support.design.widget.TabLayout;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
+    static String MY_DISPLAY_NAME = "DEFAULT";
+    private static String EMAIL;
     String BASE_URL = "http://ec2-54-173-215-12.compute-1.amazonaws.com";
     int USER_ID = -1; // this field will store the user ID
-    static String MY_DISPLAY_NAME = "DEFAULT";
     // Instantiate the RequestQueue.
     RequestQueue queue = null;
 
@@ -52,6 +44,10 @@ public class MainActivity extends AppCompatActivity {
      */
     private ViewPager mViewPager;
 
+    public static String getEmail() {
+        return EMAIL;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
         if (extras != null) {
             USER_ID = extras.getInt("USER_ID");
             MY_DISPLAY_NAME = extras.getString("MY_DISPLAY_NAME");
+            EMAIL = extras.getString("EMAIL");
         }
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -92,7 +89,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -117,52 +113,6 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            switch (position) {
-                case 0:
-                    return new PostStreamFragment();
-                case 1:
-                    return new UsersFragment();
-                case 2:
-                    return new ProfileFragment();
-            }
-            return PlaceholderFragment.newInstance(position); // should never get here
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "POSTS";
-                case 1:
-                    return "USERS";
-                case 2:
-                    return "PROFILE";
-            }
-            return null;
-        }
-    }
-
     private void getPosts(int userID) {
         // TODO: connect routes. Retrieve some posts relevant to this user. Should the user ID be an int?
         getInterests(userID);
@@ -178,10 +128,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void getUsers() {
         // TODO: show the users in the database
-    }
-
-    public static String getEmail() {
-        return MY_DISPLAY_NAME;
     }
 
     public static class PostStreamFragment extends Fragment {
@@ -233,6 +179,9 @@ public class MainActivity extends AppCompatActivity {
          */
         private static final String ARG_SECTION_NUMBER = "section_number";
 
+        public PlaceholderFragment() {
+        }
+
         /**
          * Returns a new instance of this fragment for the given section
          * number.
@@ -245,9 +194,6 @@ public class MainActivity extends AppCompatActivity {
             return fragment;
         }
 
-        public PlaceholderFragment() {
-        }
-
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
@@ -255,6 +201,51 @@ public class MainActivity extends AppCompatActivity {
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
             textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            switch (position) {
+                case 0:
+                    return new PostStreamFragment();
+                case 1:
+                    return new UsersFragment();
+                case 2:
+                    return new ProfileFragment();
+            }
+            return PlaceholderFragment.newInstance(position); // should never get here
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "POSTS";
+                case 1:
+                    return "USERS";
+                case 2:
+                    return "PROFILE";
+            }
+            return null;
         }
     }
 
